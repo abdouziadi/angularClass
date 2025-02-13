2ab8aaa49591306f84e570c327b4465421603557
abdouziedi <you@example.com>
2025-02-13 15:53:23 +0100

    lastone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  msg='hello from parent';
+  message='Bonjour';
+  image='https://tinypng.com/images/social/website.jpg'
   title = 'demo';
+  sendmessage(){
+    alert("!!!!!!!!!!!!!!!!!!!!")
+
+  }
+  msgEnfant='';
+  receiveMessage($event:string){
+    this.msgEnfant=$event;
+  }
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  @Output () msgEvent=new EventEmitter<string>();
+
+    sendMessage(){
+        this.msgEvent.emit("hello from child");
+      }
+
+
+
+    @Input('msg')message:string='';
 }
